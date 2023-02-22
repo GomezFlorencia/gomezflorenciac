9ca4dcf1e16d7e9fdb0ec212fd4367746f8302ca
gomezflorencia <florgomezstylus@gmail.com>
2023-02-22 01:05:33 -0300

    first commit

---
+++ javascript/function.js
@@ -0,0 +1,51 @@
+
+const divContainer = document.querySelector("#redessociales") ; 
+let IsClicked = true;
+ let showOrHide = function() {
+     if (IsClicked) {
+         divContainer.style.display = "block";
+         IsClicked = false;
+     } else {
+         divContainer.style.display = "none";
+         IsClicked = true;  } } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
